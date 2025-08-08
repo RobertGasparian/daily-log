7137c0d32d67d1554def39c6a32b29268cdfbdea
Robert Gasparyan <robert.gasparyan@ocado.com>
2025-08-08 13:45:14 -0400

    test for ktlint

---
+++ app/src/main/java/com/robgasp/dailylog/domain/GetDLogByIdUseCase.kt
@@ -0,0 +1,17 @@
+package com.robgasp.dailylog.domain
+
+import com.robgasp.dailylog.model.DLog
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class GetDLogByIdUseCase {
+    operator fun invoke(id: String):DLog {
+        return DLog(
+            id =     "some_id",
+            creationDate = LocalDateTime.now(),
+            modificationDate = null,
+            title = "Title 1",
+            description = "Some description bla bla blaaaaaaaaaaaa.....",
+        )
+    }
+}
