df2622e3af67fce11a4f622f43e57295bf34cdb8
Robert Gasparyan <robert.gasparyan@ocado.com>
2025-08-08 15:30:46 -0400

    normalize the namespace + test change for reviewdog

--- app/src/main/java/com/robgasp/dailylog/domain/GetDLogByIdUseCase.kt
+++ app/src/main/java/com/robgasp/dailylog/domain/GetDLogByIdUseCase.kt
@@ -3,15 +3,14 @@ package com.robgasp.dailylog.domain
 import com.robgasp.dailylog.model.DLog
 import java.time.LocalDateTime
 
-class GetDLogByIdUseCase
-
-{
-    operator  fun invoke(id: String): DLog {
+class GetDLogByIdUseCase {
+    operator fun invoke(id: String): DLog {
         return DLog(
             id = "some_id",
-            creationDate = LocalDateTime.now(),
-            modificationDate = null,
+            creationDate =     LocalDateTime.now(),
+            modificationDate =      null,
             title = "Title 1",
+
             description = "Some description bla bla blaaaaaaaaaaaa.....",
         )
     }
