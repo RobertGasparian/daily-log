0fe55d5b7ad1ca358851d4b15ad3896f4d397a9a
Robert Gasparyan <robert.gasparyan@ocado.com>
2025-09-02 19:23:57 -0400

    adding usecase for retrieving all logs with the flow

---
+++ app/src/main/java/com/robgasp/dailylog/domain/GetDLogsListUseCase.kt
@@ -0,0 +1,13 @@
+package com.robgasp.dailylog.domain
+
+import com.robgasp.dailylog.model.DLog
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GetDLogsListUseCase @Inject constructor(
+    private val dLogRepo: DLogRepository,
+){
+    operator fun invoke(): Flow<List<DLog>> = dLogRepo.getAllDLogsFlow()
+}
