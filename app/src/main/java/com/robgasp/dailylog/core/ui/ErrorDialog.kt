cdf3dee14af61ddd9e9bcd3d3e40870ff75c98e2
Robert Gasparyan <robert.gasparyan@ocado.com>
2025-09-02 19:23:23 -0400

    adding ErrorDialog as a core ui component

---
+++ app/src/main/java/com/robgasp/dailylog/core/ui/ErrorDialog.kt
@@ -0,0 +1,45 @@
+package com.robgasp.dailylog.core.ui
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun ErrorDialog(
+    modifier: Modifier = Modifier,
+    message: String = "Something went wrong! \uD83D\uDE22",
+    title: String = "Error Message",
+    actionLabel: String = "Ok",
+    cancelable: Boolean = false,
+    dismissLabel: String = "Dismiss",
+    onDismiss: () -> Unit = {},
+    onAction: () -> Unit = {}
+) {
+    AlertDialog(
+        onDismissRequest = {
+            if (cancelable) {
+                onDismiss()
+            }
+        },
+        modifier = modifier,
+        properties = DialogProperties(
+            dismissOnBackPress = cancelable,
+            dismissOnClickOutside = cancelable,
+        ),
+        confirmButton = {
+            TextButton(onClick = onAction) {
+                Text(actionLabel)
+            }
+        },
+        dismissButton = {
+            if (cancelable) TextButton(onClick = onDismiss) { Text(dismissLabel) }
+        },
+        title = { Text(title) },
+        text = { Text(message) }
+
+    )
+
+}
